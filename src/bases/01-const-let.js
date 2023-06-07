f73a018d957cb3e7a0f9e1592fe21a31cd3dab60
epsbone <109548355+epsbone@users.noreply.github.com>
2023-06-07 17:48:16 -0600

    Add files via upload

---
+++ src/bases/01-const-let.js
@@ -0,0 +1,11 @@
+const nombre = 'Esteban';
+const apellido = 'Pereda';
+
+let valorDado = 5;
+valorDado = 4;
+console.log(nombre,apellido,valorDado);
+if(true){
+    let nombre = 'Juan';
+    console.log(nombre);
+}
+console.log(valorDado);
